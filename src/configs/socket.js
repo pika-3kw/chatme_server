75ddd2851eebef3a0c1dae2873c4fe9edb905cd9
Giang Nam <giangnam10a2@gmail.com>
2021-08-14 16:32:51 +0700

    Chat private

--- src/configs/socket.js
+++ src/configs/socket.js
@@ -11,14 +11,31 @@ const createSocketIO = (httpServer) => {
 
   io.on('connect', (socket) => {
     let users = []
+    // Get all users connected
     for (let [id, socket] of io.of('/').sockets) {
-      const { username } = socket.handshake.auth
       const user = {
-        id,
-        username,
+        socketId: id,
+        username: socket.handshake.auth.username,
       }
       users.push(user)
     }
+    // User
+    const user = {
+      socketId: socket.id,
+      username: socket.handshake.auth.username,
+    }
+    // when connected, send user info to user
+    socket.emit('USER_INFO', user)
+    // when a user connected, send friends list to all users
+    io.emit('FRIENDS_LIST', users)
+    // Listening 'PRIVATE_MESSAGE' event and send message to receiver
+    socket.on('PRIVATE_MESSAGE', ({ from, content, to }) => {
+      io.to(to).emit('PRIVATE_MESSAGE', {
+        from,
+        content,
+        to,
+      })
+    })
   })
 }
 
